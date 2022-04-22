98a842b0fd47761f4b49499f38d629f7305c1dbb
Spassfabrik <83907080+Spassfabrik@users.noreply.github.com>
2022-04-22 11:46:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,37 @@ import pandas as pd
 
 from ticker import Ticker
 
+# https://builtin.com/machine-learning/streamlit-tutorial
+# https://docs.streamlit.io/library/cheatsheet
+# https://discuss.streamlit.io/t/streamlit-option-menu-is-a-simple-streamlit-component-that-allows-users-to-select-a-single-item-from-a-list-of-options-in-a-menu/20514/26
+
 spy = Ticker("spy")
-spy.SMA200
-spy.PRICE
-spy.CROSSED
-
-
 
 st.title("HFEA & 200MA Strategy WebApp")
+st.write("""
+Die Idee zu diesem Projekt stammt vom exzellenten Abenteuer des ZahlGrafs, der sich tief mit gehebelte ETFs und verschiedenen Strategien beschäftigt hat. 
+Ganz speziell hat er sich die HFEA-Strategie vorgenommen und in seiner Analysen festgestellt, dass auch eine 200-SMA-Strategie bei LETFs sinnvoll sein kann.
+Die Serie ist hier zu finden: https://www.reddit.com/r/mauerstrassenwetten/comments/s71qds/zahlgrafs_exzellente_abenteuer_teil_1/
 
+Diese WebApp soll verschiedene Funktionen anbieten, um die Handhabungen der zwei verschiedenen Strategien zu vereinfachen.
 
-st.write(f"Ticker: SPY")
-st.write(f"IS_CROSSED: {spy.CROSSED}")
-st.write(f"SMA200: {spy.SMA200}")
-st.write(f"PRICE: {spy.PRICE}")
+Folgende Funktionen sind noch geplant: 
+* Mehrere Ticker erlauben 
+* Benachrichtigung via E-Mail 
+* Benachrichtigung via mobile PushUp Notification
+* HFEA-Rebalancer-Rechner sowie Remind-Funktion
+* optional: Telegram Bot
+* optional: Einbindung von ZahlGrafs-Code zum selber ausprobieren durch Änderung von Parametern 
+""")
+
+#st.write(f"Ticker: SPY")
+#st.write(f"IS_CROSSED: {spy.CROSSED}")
+#st.write(f"SMA200: {spy.SMA200}")
+#st.write(f"PRICE: {spy.PRICE}")
+
+st.text_input("SPY")
+
+df = pd.DataFrame({"Ticker": spy, "PRICE": spy.PRICE, "SMA200": spy.SMA200, "CROSSED": spy.CROSSED})
+st.write(df)
 
 st.write("Next: E-Mail & PushUp Notfications")
